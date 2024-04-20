06f9e05a6182ffc68b276e64c07922e40d2fc7a0
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-20 21:33:54 +0200

    Add new components and update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	StyleSheet,
 	Text,
@@ -9,6 +9,8 @@ import {
 	Button,
 	Alert,
 	Platform,
+	TextInput,
+	Switch,
 } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
@@ -23,11 +25,24 @@ import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+import LoginScreen from './app/screens/LoginScreen';
+
+const categories = [
+	{ label: 'Furniture', value: 1 },
+	{ label: 'Clothing', value: 2 },
+	{ label: 'Cameras', value: 3 },
+];
 
 export default function App() {
-	return (
-		<GestureHandlerRootView style={{ flex: 1 }}>
-			<ListingsScreen />
-		</GestureHandlerRootView>
-	);
+	return <LoginScreen />;
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		justifyContent: 'center',
+	},
+});
