afc62043fa395a114137f9a9b663e3777ba60fbb
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-21 03:41:51 +0200

    Update imports in MessagesScreen.js to use new components

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { FlatList, Platform, StyleSheet, Text, View } from 'react-native';
-import ListItem from '../components/ListItem';
+import ListItem from '../components/lists/ListItem';
 import Screen from '../components/Screen';
 import colors from '../config/colors';
-import Spreator from '../components/Separator';
+import Spreator from '../components/lists/Separator';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import ListItemDeleteAction from '../components/ListItemDeleteAction';
+import ListItemDeleteAction from '../components/lists/ListItemDeleteAction';
 // import { FlatList } from 'react-native-gesture-handler';
 
 const initialMessages = [
